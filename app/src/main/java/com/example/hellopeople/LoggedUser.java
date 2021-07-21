be48b699790ac6ef3659b180d13cf7373d3931a3
GuilhermePalma <guilherme.palma8@etec.sp.gov.br>
2021-07-21 16:30:25 -0300

    Create: Implementação da Busca do IP, Dados Derivados e 'Hello' na Linguagem Local

--- app/src/main/java/com/example/hellopeople/LoggedUser.java
+++ app/src/main/java/com/example/hellopeople/LoggedUser.java
@@ -2,18 +2,35 @@ package com.example.hellopeople;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class LoggedUser extends AppCompatActivity {
 
     private String name;
     private String password;
 
+    private String ip_user = "";
+    private String hello = "";
+
+
     private final String NAME_PREFERENCES = "LOGIN";
     private final String NAME_LOGIN = "NAME";
     private final String PASSWORD_LOGIN = "PASSWORD";
+
+    private static final String URL_GET_IP = "http://checkip.amazonaws.com";
+    private static final String URL_GET_DATA = "http://ip-api.com/json";
+    private static final String URL_GET_HELLO = "https://fourtonfish.com/hellosalut";
+
+    SearchInternet researches;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,9 +38,7 @@ public class LoggedUser extends AppCompatActivity {
         setContentView(R.layout.activity_logged_user);
 
         getData();
-
         searchMessage();
-
 
     }
 
@@ -36,7 +51,56 @@ public class LoggedUser extends AppCompatActivity {
         User user = new User(name,password);
     }
 
+
+
     private void searchMessage() {
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+        Handler handler = new Handler(Looper.getMainLooper());
+
+        executor.execute(() -> {
+
+            // obtem o Ip
+            ip_user = getIp();
+
+            dataPersonIp(ip_user);
+            helloForIp(ip_user);
+
+            handler.post(() -> {
+                // Altera na Atividade Principal
+            });
+        });
+
+    }
+
+    // Obtem o IP
+    private String getIp(){
+        return SearchInternet.searchByUrl(URL_GET_IP, "GET");
+    }
+
+    private String dataPersonIp(String ip){
+
+        final String PARAMETER_FIELD = "fields";
+        final String fields = "status,message,country,countryCode,region," +
+                "regionName,city,timezone,org,mobile,query";
+
+            Uri buildURI = Uri.parse(URL_GET_DATA).buildUpon()
+                    .appendPath(ip)
+                    .appendQueryParameter(PARAMETER_FIELD, fields)
+                    .build();
+
+            return SearchInternet.searchByUrl(buildURI.toString(), "GET");
+    }
+
+    private String helloForIp(String ip){
+
+        final String PARAMETER_IP = "ip";
+
+        Uri uriBuild = Uri.parse(URL_GET_HELLO).buildUpon()
+                .appendQueryParameter(PARAMETER_IP, ip)
+                .build();
+
+        return SearchInternet.searchByUrl(uriBuild.toString(), "GET");
+
     }
 
 }
--- app/src/main/java/com/example/hellopeople/SearchInternet.java
+++ app/src/main/java/com/example/hellopeople/SearchInternet.java
@@ -1,19 +1,83 @@
 package com.example.hellopeople;
 
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class SearchInternet {
 
-    private static String IP_URL = "http://ip-api.com/json/";
-    private static String FIELD_IP = "fields";
+    public static String searchByUrl(String url, String method){
 
-    private void getIp(){
+        String result_search;
+        HttpURLConnection connection = null;
+        BufferedReader reader = null;
+
+        try {
+
+            URL url_web = new URL(url);
+
+            // Configura a Conexão com a Internet
+            connection = (HttpURLConnection) url_web.openConnection();
+            connection.setRequestMethod(method);
+            connection.connect();
+
+            InputStream inputStream = connection.getInputStream();
+
+            if (inputStream != null){
+
+                // Obtem uma Leitura do Resultado da Internet
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                // Irão Recuperar os Resultados
+                StringBuffer bufferResponse = new StringBuffer();
+                String line;
+
+                //Loop para a Leitura Linha por Linha
+                while ((line = reader.readLine()) != null) {
+                    bufferResponse.append(line);
+                }
+
+                //Buffer sem Informações
+                if (bufferResponse.length() == 0) return null;
+
+                result_search = bufferResponse.toString();
+
+            } else return null;
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+            Log.e("ERROR FORMED URL", "Error in URL Formation\n" + e);
+            return null;
+        } catch (IOException e) {
+            e.printStackTrace();
+            Log.e("ERROR READ URL", "Error reading URL Connection\n" + e);
+            return null;
+        } finally {
+            //Fecha a Conexão
+            if (connection != null) {
+                connection.disconnect();
+            }
+            //Fecha o Reader
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        Log.i("RESULT SEARCH", result_search);
+
+        return result_search;
+
     }
 
-    private void getConfiguration(){
-
-    }
-
-    private void getHello(){
-
-    }
 
 }
